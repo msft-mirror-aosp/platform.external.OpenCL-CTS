8282a74336f71270d0f1c6c9302a1613f5a3c6c4
Ankit Goyal <layog@google.com>
2020-09-29 19:07:11 +0800

    Add support to enumerate subtests
    Fix: 160938400
    Test: Manually tested
    Change-Id: I0cc91dec0d38c6970cd9fcd955f1c3582b9937b6

--- scripts/test_opencl_cts.py
+++ scripts/test_opencl_cts.py
@@ -2,14 +2,11 @@
 
 import unittest
 import os
-from xml.etree import ElementTree
+import shlex
 import subprocess
-from functools import partial
-import re
 import sys
 
 ANDROID_RUNNER_REQUIRED_VERBOSITY = 2
-TEST_CONFIG = os.path.join(os.path.dirname(__file__), "test_opencl_cts.xml")
 
 
 def run_command(command):
@@ -22,43 +19,69 @@ def run_command(command):
   return ret.returncode, ret.stdout, ret.stderr
 
 
+def get_subtests(binary_path):
+  retcode, output, _ = run_command(shlex.split(f'{binary_path} --help'))
+
+  test_name_line = "Test names"
+  index = output.find(test_name_line)
+  if index == -1:
+    return []
+
+  test_names_output = output[index:]
+  test_names = []
+  # Skip the first line which starts with "Test names"
+  for test_name in test_names_output.splitlines()[1:]:
+    if not test_name.startswith((" ", "\t")):
+      break
+    test_names.append(test_name.strip())
+
+  return test_names
+
+
 class OpenCLTest(unittest.TestCase):
 
   def __init__(self, test_name, binary_path, args):
+
     self._test_name = test_name
     self._binary_path = binary_path
     self._args = args
 
-    self._command = list(map(str.strip, [self._binary_path] + self._args))
-    self.test_func_name = f"test_{self._test_name}"
+    self._command = list(
+        map(str.strip, [self._binary_path, self._test_name] + self._args))
+    self.test_func_name = self._test_name
 
     setattr(self, self.test_func_name, self.genericTest)
     super().__init__(methodName=self.test_func_name)
 
   def genericTest(self):
     retcode, output, oerror = run_command(self._command)
+
+    # TODO(layog): CTS currently return non-zero return code if the
+    # implementation is missing for some API even if the API is not supported by
+    # the version reported by the driver. Need to patch upstream.
+    missing_line = f"ERROR: Test '{self._test_name}' is missing implementation"
+    if missing_line in output or missing_line in oerror:
+      self.skipTest(f"{self._test_name} API not available in the driver")
+
     self.assertFalse(retcode, "Test exited with non-zero status")
 
     # TODO(b/158646251): Update upstream to exit with proper error code
-    statline_regex = re.compile("^passed \d+ of \d+ tests.")
-    passed = total = None
-    for line in reversed(output.split("\n")):
-      if statline_regex.match(line.strip().lower()):
-        _, passed, _, total, *_ = line.strip().split()
-        passed = int(passed)
-        total = int(total)
-
-    self.assertTrue(passed is not None, "Couldn't find status line")
-    self.assertTrue(total is not None, "Couldn't find status line")
-    self.assertEqual(passed, total, "{} subtests failed".format(total - passed))
+    passed_line = "PASSED test."
+    self.assertTrue(passed_line in output)
 
 
 def main():
   """main entrypoint for test runner"""
   _, test_name, binary_path, *args = sys.argv
 
+  # HACK: Name hack to report the actual test name
+  OpenCLTest.__name__ = test_name
+  OpenCLTest.__qualname__ = test_name
+
   suite = unittest.TestSuite()
-  suite.addTest(OpenCLTest(test_name, binary_path, args))
+  subtests = get_subtests(binary_path)
+  for subtest in subtests:
+    suite.addTest(OpenCLTest(subtest, binary_path, args))
 
   runner = unittest.TextTestRunner(
       stream=sys.stderr, verbosity=ANDROID_RUNNER_REQUIRED_VERBOSITY)

5e7cc09e2d935115a36aa219711b907bb3aca52d
Ankit Goyal <layog@google.com>
2020-10-07 12:29:38 +0800

    Add subtests filtering and proper argument parsing
    Bug: 160842454
    Test: Manually tested
    Change-Id: Ibe4586d46f0fee66161ac872ccf95ece35a2a298

--- scripts/test_opencl_cts.py
+++ scripts/test_opencl_cts.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
+import argparse
 import os
 import pipes
 import subprocess
@@ -9,6 +10,23 @@ import sys
 import unittest
 
 ANDROID_RUNNER_REQUIRED_VERBOSITY = 2
+
+
+def parse_args(args: Optional[List[str]] = None) -> argparse.Namespace:
+  parser = argparse.ArgumentParser()
+  parser.add_argument('test_name', type=str, help="Name of the test")
+  parser.add_argument('binary_path', type=str,
+                      help="Full path to the binary on device")
+  parser.add_argument('--subtests', type=str, nargs='*',
+                      help="Specific subtests to run")
+  parser.add_argument('--test_args', type=str, nargs='*',
+                      help="Unfiltered arguments to pass to the run command")
+
+  args = parser.parse_args(args)
+  args.subtests = args.subtests or []
+  args.test_args = args.test_args or []
+
+  return args
 
 
 def run_command(command: str) -> Tuple[int, str, str]:
@@ -22,7 +40,7 @@ def run_command(command: str) -> Tuple[int, str, str]:
   return ret.returncode, ret.stdout, ret.stderr
 
 
-def get_subtests(binary_path: str) -> List[str]:
+def get_all_subtests(binary_path: str) -> List[str]:
   retcode, output, _ = run_command(f'{binary_path} --help')
 
   test_name_line = "Test names"
@@ -39,6 +57,22 @@ def get_subtests(binary_path: str) -> List[str]:
     test_names.append(test_name.strip())
 
   return test_names
+
+
+def get_subtests(binary_path: str, subtests: List[str]) -> List[str]:
+  all_subtests = set(get_all_subtests(binary_path))
+  if not subtests:
+    return all_subtests
+
+  subtests = set(subtests)
+  selected_subtests = subtests & all_subtests
+  remaining_subtests = subtests - all_subtests
+
+  if remaining_subtests:
+    print("Could not find subtests: {}".format(', '.join(remaining_subtests)),
+          file=sys.stderr)
+
+  return sorted(list(selected_subtests))
 
 
 class OpenCLTest(unittest.TestCase):
@@ -78,16 +112,16 @@ class OpenCLTest(unittest.TestCase):
 
 def main():
   """main entrypoint for test runner"""
-  _, test_name, binary_path, *args = sys.argv
+  args = parse_args(sys.argv[1:])
 
   # HACK: Name hack to report the actual test name
-  OpenCLTest.__name__ = test_name
-  OpenCLTest.__qualname__ = test_name
+  OpenCLTest.__name__ = args.test_name
+  OpenCLTest.__qualname__ = args.test_name
 
   suite = unittest.TestSuite()
-  subtests = get_subtests(binary_path)
+  subtests = get_subtests(args.binary_path, args.subtests)
   for subtest in subtests:
-    suite.addTest(OpenCLTest(subtest, binary_path, args))
+    suite.addTest(OpenCLTest(subtest, args.binary_path, args.test_args))
 
   runner = unittest.TextTestRunner(
       stream=sys.stderr, verbosity=ANDROID_RUNNER_REQUIRED_VERBOSITY)

9f40b6d31bd34d806069d7947a0044fae5c1700f
Romaric Jodin <rjodin@google.com>
2025-09-01 14:32:55 +0200

    make timeouts configurable
    Bug: None
    Test: atest OpenCL-CTS-test_basic
    Change-Id: Ic9ea991e7e00a84ed332a16779e3f7d906a41947

--- android/generate_xml_files.py
+++ android/generate_xml_files.py
@@ -43,6 +43,60 @@ def get_all_tests():
       all_tests[executable].append(dict(args=args, subtests=subtests, path=executable_and_args[0]))
   return all_tests
 
+timeouts = {
+  'OpenCL-CTS-test_allocations' : 10,
+  'OpenCL-CTS-test_basic': {
+    'astype': 10,
+    'async_copy_global_to_local': 10,
+    'async_copy_global_to_local2D': 10,
+    'async_copy_global_to_local3D': 10,
+    'async_copy_local_to_global': 10,
+    'async_copy_local_to_global2D': 10,
+    'async_copy_local_to_global3D': 10,
+    'async_strided_copy_global_to_local': 10,
+    'async_strided_copy_local_to_global': 10,
+    'hiloeo': 10,
+    'imagedim_non_pow2': 20,
+    'imagedim_pow2': 20,
+    'prefetch': 2,
+    'vector_creation': 20,
+    'vector_swizzle': 5,
+    'vload_constant': 2,
+    'vload_global': 2,
+    'vload_local': 2,
+    'vload_private': 2,
+    'vstore_global': 2,
+    'vstore_local': 2,
+    'vstore_private': 2,
+  },
+  'OpenCL-CTS-test_buffers': 2,
+  'OpenCL-CTS-test_c11_atomics': 10,
+  'OpenCL-CTS-test_cl_copy_images': 30,
+  'OpenCL-CTS-test_cl_fill_images': 30,
+  'OpenCL-CTS-test_cl_read_write_images': 10,
+  'OpenCL-CTS-test_image_streams': 10,
+  'OpenCL-CTS-test_integer_ops': 20,
+  'OpenCL-CTS-test-kernel_image_methods': 10,
+  'OpenCL-CTS-test_multiples': {
+    'context_multiple_contexts_same_device': 20,
+  },
+  'OpenCL-CTS-test_relationals': {
+    'shuffle_array_cast': 10,
+    'shuffle_built_in': 10,
+    'shuffle_built_in_dual_input': 10,
+    'shuffle_copy': 10,
+    'shuffle_function_call': 10,
+  },
+  'OpenCL-CTS-test_samplerless_reads': 10,
+  'OpenCL-CTS-test_vectors': {
+    'vec_align_array': 20,
+    'vec_align_packed_struct': 20,
+    'vec_align_packed_struct_arr': 20,
+    'vec_align_struct': 20,
+    'vec_align_struct_arr': 20,
+  }
+}
+
 def generate_xml(binary, tests, timeout):
   def create_subelement_with_attribs(element, tag, attribs):
     subelement = ElementTree.SubElement(element, tag)
@@ -68,12 +122,17 @@ def generate_xml(binary, tests, timeout):
           'value': test[1],
         })
 
-  def generate_test_rules(configuration, tests, timeout):
+  def generate_test_rules(configuration, binary, tests, default_timeout):
     for test in tests:
+      if type(timeouts.get(binary)) is dict:
+        timeout = timeouts[binary].get(test[0], default_timeout)
+      else:
+        timeout = timeouts.get(binary, default_timeout)
+
       test_rule = create_subelement_with_attribs(
         configuration, 'test', { 'class': "com.android.tradefed.testtype.binary.ExecutableTargetTest" })
       create_subelement_with_attribs(
-        test_rule, 'option', { 'name': "per-binary-timeout", 'value': timeout })
+        test_rule, 'option', { 'name': "per-binary-timeout", 'value': "{}m".format(timeout) })
       create_subelement_with_attribs(
         test_rule, 'option', { 'name': "test-command-line", 'key' : test[0], 'value': test[1] })
 
@@ -83,7 +142,7 @@ def generate_xml(binary, tests, timeout):
   logcat.attrib['name'] = "logcat-on-failure"
   logcat.attrib['value'] = "false"
   generate_push_file_rules(configuration, [(binary, DATA_PATH)])
-  generate_test_rules(configuration, tests, timeout)
+  generate_test_rules(configuration, binary, tests, timeout)
   stringified_configuration = ElementTree.tostring(configuration, 'utf-8')
   reparsed_configuration = minidom.parseString(stringified_configuration)
   with open(os.path.join(SCRIPT_DIR, binary + ".xml"), 'w') as f:
@@ -123,9 +182,9 @@ def main():
         if args != " full*":
           subtest = args + subtest
         subtests.append((clean_name(subtest).removeprefix("_"), "CL_WIMPY_MODE=1 " + prefix + binary_path + subtest + suffix))
-    generate_xml(binary, subtests, "30m")
+    generate_xml(binary, subtests, 1)
 
-  generate_xml("OpenCL-CTS", cts_tests, "120m")
+  generate_xml("OpenCL-CTS", cts_tests, 180)
 
 if __name__ == '__main__':
   main()
